94f954c4481f240fd218f01921ed8d8b6b20856c
yann-yvan <yann39@live.com>
2019-03-05 12:46:47 +0100

    development in progress

--- config/dohone.php
+++ config/dohone.php
@@ -1,5 +1,17 @@
 <?php
 
 return [
-    //
+    /*
+     * Start transaction
+     */
+    'start' => [
+        'rH' => 'XXXXXXXX',
+        'rDvs' => 'XAF',
+        'source' => env('APP_NAME'),
+        'logo' => asset('assets/images/logo.png'),
+        "endPage" => "https://www.e-afrika.fr",
+        "notifyPage" => "https://www.e-afrika.fr",
+        "cancelPage" => "https://www.e-afrika.fr",
+        "cmd" => "start"
+    ]
 ];
--- src/Dohone.php
+++ src/Dohone.php
@@ -4,5 +4,16 @@ namespace paymentCm\Dohone;
 
 class Dohone
 {
-    // Build wonderful things
+    public static function init_payment($phone, $amount, $email, $reason = null, $lang = 'default', $name = "Client")
+    {
+        $data = array_merge(config('dohone.start'),[
+            'rN' => $name,
+            'rLocale' => (strcmp($lang, 'default') == 0 ? config('dohone.start.logo') : $lang),
+            'rT' => $phone,
+            'rH' => $amount,
+            'rE' => $email,
+            'rI' => $reason
+        ]);
+        dd($data);
+    }
 }
